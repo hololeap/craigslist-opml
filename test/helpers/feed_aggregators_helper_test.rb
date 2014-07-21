a8d9544e9b09e47043630a8a24b66d3a973c7b53
hololeap <hololeap@gmail.com>
2014-07-21 06:46:29 -0600

    Removed /test directory

--- test/helpers/feed_aggregators_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class FeedAggregatorsHelperTest < ActionView::TestCase
-end
--- test/models/search_value_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class SearchValueTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
